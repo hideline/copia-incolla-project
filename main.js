d63a02a7eeb68769075e649b8191a3a91981c5ac
Marco Arnulfo <arnulfomarco7@gmail.com>
2024-04-15 18:31:40 +0200

    refactor functions

--- main.js
+++ main.js
@@ -27,11 +27,33 @@ const buySecondPlan = document.querySelector('#buy-second-plan')
 const thirdCard = document.querySelector('#third-card')
 const buyThirdPlan = document.querySelector('#buy-third-card')
 
+function spegniLed() {
+    firstCard.classList.remove('bg-white/5')
+    firstCard.classList.remove('ring-2')
+    firstCard.classList.remove('ring-indigo-500')
+    buyPlan.classList.add('bg-white/10')
+    buyPlan.classList.remove('bg-indigo-500')
+
+    secondCard.classList.remove('bg-white/5')
+    secondCard.classList.remove('ring-2')
+    secondCard.classList.remove('ring-indigo-500')
+    buySecondPlan.classList.add('bg-white/10')
+    buySecondPlan.classList.remove('bg-indigo-500')
+
+    thirdCard.classList.remove('bg-white/5')
+    thirdCard.classList.remove('ring-2')
+    thirdCard.classList.remove('ring-indigo-500')
+    buyThirdPlan.classList.add('bg-white/10')
+    buyThirdPlan.classList.remove('bg-indigo-500')
+}
+
 
 
 function primaProva() {
-    spotlightFirst()
+    spegniLed()
+    secondCard.classList.add('ring-1', 'ring-white/10')
     firstCard.classList.add('bg-white/5')
+    firstCard.classList.remove('ring-1', 'ring-white/10')
     firstCard.classList.add('ring-2')
     firstCard.classList.add('ring-indigo-500')
     buyPlan.classList.remove('bg-white/10')
@@ -39,66 +61,37 @@ function primaProva() {
 }
 
 function secondaProva() {
-    spotlightSecond()
+    spegniLed()
     secondCard.classList.add('bg-white/5')
+    secondCard.classList.remove('ring-1', 'ring-white/10')
     secondCard.classList.add('ring-2')
     secondCard.classList.add('ring-indigo-500')
     buySecondPlan.classList.remove('bg-white/10')
     buySecondPlan.classList.add('bg-indigo-500')
-
+    firstCard.classList.remove('ring-2')
+    firstCard.classList.remove('ring-indigo-500')
+    firstCard.classList.add('ring-1', 'ring-white/10')
+    thirdCard.classList.remove('ring-2')
+    thirdCard.classList.remove('ring-indigo-500')
+    thirdCard.classList.add('ring-1', 'ring-white/10')
 }
 
 function terzaProva() {
-    spotlightThird()
+    spegniLed()
     thirdCard.classList.add('bg-white/5')
+    thirdCard.classList.remove('ring-1', 'ring-white/10')
     thirdCard.classList.add('ring-2')
     thirdCard.classList.add('ring-indigo-500')
     buyThirdPlan.classList.remove('bg-white/10')
     buyThirdPlan.classList.add('bg-indigo-500')
-}
-
-function spotlightFirst() {
-    secondCard.classList.remove('bg-white/5')
-    secondCard.classList.remove('ring-2')
-    secondCard.classList.remove('ring-indigo-500')
-    buySecondPlan.classList.add('bg-white/10')
-    buySecondPlan.classList.remove('bg-indigo-500')
-
-    thirdCard.classList.remove('bg-white/5')
-    thirdCard.classList.remove('ring-2')
-    thirdCard.classList.remove('ring-indigo-500')
-    buyThirdPlan.classList.add('bg-white/10')
-    buyThirdPlan.classList.remove('bg-indigo-500')
-}
-function spotlightSecond() {
-    firstCard.classList.remove('bg-white/5')
     firstCard.classList.remove('ring-2')
     firstCard.classList.remove('ring-indigo-500')
-    buyPlan.classList.add('bg-white/10')
-    buyPlan.classList.remove('bg-indigo-500')
-
-    thirdCard.classList.remove('bg-white/5')
-    thirdCard.classList.remove('ring-2')
-    thirdCard.classList.remove('ring-indigo-500')
-    buyThirdPlan.classList.add('bg-white/10')
-    buyThirdPlan.classList.remove('bg-indigo-500')
-
-}
-function spotlightThird() {
-    firstCard.classList.remove('bg-white/5')
-    firstCard.classList.remove('ring-2')
-    firstCard.classList.remove('ring-indigo-500')
-    buyPlan.classList.add('bg-white/10')
-    buyPlan.classList.remove('bg-indigo-500')
-
-    secondCard.classList.remove('bg-white/5')
+    firstCard.classList.add('ring-1', 'ring-white/10')
     secondCard.classList.remove('ring-2')
     secondCard.classList.remove('ring-indigo-500')
-    buySecondPlan.classList.add('bg-white/10')
-    buySecondPlan.classList.remove('bg-indigo-500')
-
-
+    secondCard.classList.add('ring-1', 'ring-white/10')
 }
+
 
 const monthly = document.querySelector('#monthly')
 const annually = document.querySelector('#annually')
